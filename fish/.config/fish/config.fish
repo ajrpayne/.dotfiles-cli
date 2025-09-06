cf2e5d49b05d8af33e7399204fddc8703dadbe8a
ajrpayne <ajrpayne@gmail.com>
2025-09-05 23:42:44 -0700

    update shell and starship

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    abbr -a -- k kubectl
+    if test -z (pgrep ssh-agent)
+        ssh-agent -c | sed 's/^echo/#echo/' >$SSH_ENV
+        chmod 600 $SSH_ENV
+        source $SSH_ENV >/dev/null
+    else
+        source $SSH_ENV >/dev/null
+    end
 end
 starship init fish | source
